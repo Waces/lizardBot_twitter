0759ceee144b723e4a01cf1d24047eca532de852
Waces <31547648+Waces@users.noreply.github.com>
2018-01-08 11:57:13 -0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -15,18 +15,18 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 path = "images/"
-im = ( path + "/" + random.choice(os.listdir(path)) )
+im = ( path + "/" + random.choice(os.listdir(path)) ) # Random image [wip?]
 
 
 hourMin = 60
-# 60 for Mins, 3600 for hours, 1 for seconds
+# 60 Mins, 3600 hours, 1 seconds
 
 
-def poolRemove():
+def poolRemove(): # Remove/Move from the 'not used' text list to 'used' text list
     texts.archived.append(txt)
     texts.text.remove(txt)
 
-def poolReset():
+def poolReset(): # When 'non used' text list is empty, will get again the the lines from the 'used' list
     texts.text.extend(texts.archived)
     texts.archived.clear()
     #print('---')
@@ -36,15 +36,15 @@ def tweet():
 
 # Main loop
 while 1 == 1:
-    sleepTime = random.randrange(1, 5)*hourMin
+    sleepTime = random.randrange(1, 5)*hourMin # 'Loop time' range
 
-    if len(texts.text) != 0:
-        txt = random.choice(texts.text)
+    if len(texts.text) != 0: # Only pick text if list is not empty
+        txt = random.choice(texts.text) # Pick random text from 'not used' text list
         poolRemove()
     else:
         poolReset()
         txt = random.choice(texts.text)
         poolRemove()
-    #tweet()
-    print(txt)
-    time.sleep(sleepTime)
+    # tweet()
+    print(txt) # Just for the sake of debugging 
+    time.sleep(sleepTime) # Loop 'timer'
