077992a5d240a61918e8eb8c3d82e731ddcc5d98
Waces <31547648+Waces@users.noreply.github.com>
2018-01-08 11:58:07 -0300

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,14 +1,15 @@
+# Not used text
 text = [
 
     "Look at this beauty",
     "This is such a lovely creature",
     "Take your time to appreciate this lizard",
     "This is the definition of perfect",
-    "Isn't it beautiful!?",
-    ""
+    "Isn't it beautiful!?"
 
 ]
 
+# Used text
 archived = [
 
 
