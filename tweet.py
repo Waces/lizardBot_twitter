5d741b8ce31b4c496bdb4da71fe7b07701364843
Waces <31547648+Waces@users.noreply.github.com>
2018-01-10 12:52:28 -0300

    Automatize downloading and deleting images

--- tweet.py
+++ tweet.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 from keys import keys
 import texts
+import scrapper
 import random
 import os
 
@@ -14,37 +15,61 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-path = "images/"
-im = ( path + "/" + random.choice(os.listdir(path)) ) # Random image [wip?]
+path = "images"
 
 
 hourMin = 60
-# 60 Mins, 3600 hours, 1 seconds
+# 60 for Mins, 3600 for hours, 1 for seconds
 
+# 90, 120 - 1h30m to 2h
+# 60,90 - 1h to 1h30m
 
-def poolRemove(): # Remove/Move from the 'not used' text list to 'used' text list
+def poolRemove():
     texts.archived.append(txt)
     texts.text.remove(txt)
 
-def poolReset(): # When 'non used' text list is empty, will get again the the lines from the 'used' list
+def poolReset():
     texts.text.extend(texts.archived)
     texts.archived.clear()
-    #print('---')
+
 
 def tweet():
     api.update_with_media(im, txt)
 
 # Main loop
 while 1 == 1:
-    sleepTime = random.randrange(1, 5)*hourMin # 'Loop time' range
+    sleepTime = random.randrange(3, 5)*hourMin
+    im = (path + "/" + random.choice(os.listdir(path)))
 
-    if len(texts.text) != 0: # Only pick text if list is not empty
-        txt = random.choice(texts.text) # Pick random text from 'not used' text list
+    if len(texts.text) != 0:
+        txt = random.choice(texts.text)
         poolRemove()
     else:
         poolReset()
         txt = random.choice(texts.text)
         poolRemove()
-    # tweet()
-    print(txt) # Just for the sake of debugging 
-    time.sleep(sleepTime) # Loop 'timer'
+    tweet()
+    print("Text:",txt)  # Just for the sake of debugging
+    print("Image:", im)
+    print("Downloaded:", scrapper.downFile)
+
+    # print("---")
+    try:
+        scrapper.download() # Download one random image from Google
+    except:
+        print("Download Failed")
+    scrapper.imn = len(os.listdir(path))+1
+    imi = scrapper.imId			            # From here
+    scrapper.imId = random.randrange(0, 101)
+    if scrapper.imId == imi:
+        scrapper.imId += 1                          
+        if scrapper.imId > 100:
+            scrapper.imId -= random.randrange(3, 6)
+        elif scrapper.imId < 0:
+            scrapper.imId -= random.randrange(3, 6) # To here is to make sure it doesn't pick the same, yeh, there are a lot better ways :P
+    scrapper.downFile = scrapper.images[scrapper.imId]['ou']
+    toRem = (path + "/" + random.choice(os.listdir(path))) # Select one random image to delete
+    os.system("rm %s" %toRem) # Deletes the image
+    print("Deleted:", toRem)
+    print("---")
+    time.sleep(sleepTime)
