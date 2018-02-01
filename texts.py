26b77d26b1c428db9fd5c384f32493af567002e3
Waces <31547648+Waces@users.noreply.github.com>
2018-02-01 08:40:36 -0300

    added 3 more phrases :P

--- texts.py
+++ texts.py
@@ -6,7 +6,9 @@ text = [
     "Take your time to appreciate this lizard",
     "This is the definition of perfect",
     "Isn't it beautiful!?"
-
+    "Lizards are precious creatures that deserve a lot of love 🦎",
+    "L🦎I🦎Z🦎A🦎R🦎D🦎S🦎",
+    "Give your love to this creature"
 ]
 
 # Used text
